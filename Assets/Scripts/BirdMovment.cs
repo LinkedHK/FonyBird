e7ebf86d5aaebc5969dde8c55fdabc957b7401fb
LinkedHK <henryproface@gmail.com>
2015-01-11 20:06:12 +0800

    Migrating to GitHub

---
+++ Assets/Scripts/BirdMovment.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovment : MonoBehaviour {
+
+	// Use this for initialization
+
+    
+    public Vector3 gravity;
+    public Vector3 flapVelocity;
+    public float maxSpeed = 5f;
+    public float forwardSpeed = 1f;
+    bool didFlap = false;
+    Vector3 velocety = Vector3.zero;
+
+	void Start () {
+        //transform.position = new Vector3(0f, 0f, camera.ScreenToWorldPoint(new Vector3(0f, 0f, 1f)).z);
+       
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+
+        // Do graphic and input updates here.
+
+        // GetMouseButtonDown 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            didFlap = true;
+        }
+
+	}
+
+    // Physics here
+    void FixedUpdate()
+    {
+
+        calculateFallVelocety();
+        transform.rotation =  calculateRotation();
+   
+     
+    }
+    Quaternion calculateRotation()
+    {
+        float angle = 0;
+        if (velocety.y < 0)
+        {
+            angle = Mathf.Lerp(0, -90, -velocety.y / maxSpeed);
+        }
+        return Quaternion.Euler(0, 0, angle);
+    }
+    void calculateFallVelocety()
+    { 
+        velocety.x = forwardSpeed;
+        // Fall velocety
+        velocety += gravity * Time.deltaTime;
+        // Forward velocety
+       
+        if (didFlap == true)
+        {
+            didFlap = false;
+            if (velocety.y < 0)
+                velocety.y = 0;
+            velocety += flapVelocity;
+        }
+        
+        velocety = Vector3.ClampMagnitude(velocety, maxSpeed);
+        transform.position += velocety * Time.deltaTime;
+       
+    }
+
+}
---
+++ Assets/Scripts/CamerTrackPlayer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerTrackPlayer : MonoBehaviour {
+
+    Transform player;
+    Vector3 offset;
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+	// Use this for initialization
+	void Start () {
+
+        GameObject player_obj = GameObject.FindGameObjectWithTag("Player");
+
+        if (player_obj == null)
+        {
+            Debug.LogError("Couldn't find an object with tag player!");
+            return;
+        }
+        player = player_obj.transform;
+        // calculate initial camera position
+        offset = transform.position - player.position;
+	
+	}
+
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 pos = transform.position;
+            pos.x = player.position.x + offset.x;
+            transform.position = pos;
+        }
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+
+        if (player != null)
+        {
+            Vector3 pos = transform.position;
+
+
+            transform.position = new Vector3(Mathf.Abs(offsetX), transform.position.y, transform.position.z);
+
+        }
+
+        */
+    }
+}
